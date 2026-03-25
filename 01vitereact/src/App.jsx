0ac049e9813967b889b6c3c77438fd511f48ee56
Utkarsh Gupta <98682354+Utk7gpt@users.noreply.github.com>
2026-03-25 05:36:07 +0000

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,8 +2,13 @@ import Chai from "./chai";
 
 
 function App() {
+  const username = "with hitesh sir"
   return (
+    <>
     <Chai />
+    <h1>chai aur  react {username}</h1>
+    <p>test para</p>
+    </>
   );
   
  }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App.jsx'
+
+function MyApp(){
+  return(
+    <div>
+      <h1>My custom react app</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href ="https://www.google.com">Google</a>
+)
+
+const anotheruser = " hel  lo lets learn react"
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://www.google.com'},
+  'Click me to visit google',
+   anotheruser
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+reactElement
 )
+
+  
+ 
+
   
